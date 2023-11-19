a323b04bf7088958b3cddcb00b39a7a91a820d24
shaqdeff <shaquillendunda@gmail.com>
2023-11-19 22:21:46 +0300

    Set up middleware for internalization

---
+++ src/middleware.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+
+let defaultLocale = 'en';
+let locales = ['en', 'sw', 'fr', 'es', 'cn'];
+
+// Get the preferred locale, similar to above or using a library
+function getLocale(request: NextRequest) {
+  const acceptedLanguage = request.headers.get('accept-language') ?? undefined;
+  let headers = { 'accept-language': acceptedLanguage };
+  let languages = new Negotiator({ headers }).languages();
+
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const pathname = request.nextUrl.pathname;
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathname}`, request.url)
+    );
+  }
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next, assets, api)
+    '/((?!api|assets|.*\\..*|_next).*)',
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
